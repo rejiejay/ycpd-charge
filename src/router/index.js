7276d9bbdd7f99fb6f80c6f4b74d9c955111ae9a
Rejiejay <454766952@qq.com>
2018-06-12 11:10:06 +0800

    update to list

--- src/router/index.js
+++ src/router/index.js
@@ -6,20 +6,14 @@ Vue.use(Router);
 const routes = [
   {
     path: '/index',
-    alias: ['/home', '/'],
-    name: 'home',
-    component: () => import('@/router/home/index'), // 主页 首屏
+    alias: ['/list', '/'],
+    name: 'list',
+    component: () => import('@/router/list/index'), // 列表
     meta: { title: '充电桩' },
   }, {
-    path: '/search/index',
-    alias: ['/search'],
-    name: 'search',
-    component: () => import('@/router/search/index'), // 搜索
-    meta: { title: '搜索' },
-  }, {
-    path: '/search/detail',
-    name: 'searchDetail',
-    component: () => import('@/router/search/detail'), // 列表详情
+    path: '/list/detail',
+    name: 'listDetail',
+    component: () => import('@/router/list/detail'), // 列表详情
     meta: { title: '列表详情' },
   },
 ];
---
+++ src/router/list/ajaxs.js
@@ -0,0 +1,6 @@
+import RequestedUrl from './../../config/RequestedUrl.js';
+
+const ajaxs = {
+}
+
+export default ajaxs;
