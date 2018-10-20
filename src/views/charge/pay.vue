ce54a90cb271eb4628a6215fa2364e8719dcccc9
Rejiejay <454766952@qq.com>
2018-10-20 15:38:13 +0800

    init pay

---
+++ src/views/charge/pay.vue
@@ -0,0 +1,47 @@
+<!-- 充电中 -->
+<template>
+<div class="pay-select">
+
+</div>
+</template>
+
+<script>
+
+export default {
+    name: 'pay-select',
+
+    components: {
+        RadialProgressBar, // 充电百分比的圈圈
+    },
+
+    data () {
+        return {
+            clientWidth: document.body.offsetWidth || document.documentElement.clientWidth || window.innerWidth, // 设备的宽度
+            clientHeight: document.body.offsetHeight || document.documentElement.clientHeight || window.innerHeight, // 设备高度
+        }
+    },
+
+    mounted: function () { },
+
+    methods: {
+    },
+}
+
+</script>
+
+<style scoped lang="less">
+@black1: #303133;
+@black2: #606266;
+@black3: #909399;
+@black4: #C0C4CC;
+
+.pay-select {
+    position: relative;
+    width: 100%;
+    font-size: 14px;
+    min-height: 100%;
+    background-color: #f5f5f5;
+}
+
+
+</style>
